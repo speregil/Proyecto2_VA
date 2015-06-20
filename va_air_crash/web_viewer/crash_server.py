f49ca0baff4b623d632e8a32deb264087fba2ffb
speregil <s.gilparga@gmail.com>
2015-06-20 18:20:04 -0500

    Busca todos los accidentes en una ciudad. Falta que retorne todos los datos, solo retorna el conteo

--- va_air_crash/web_viewer/crash_server.py
+++ va_air_crash/web_viewer/crash_server.py
@@ -6,6 +6,7 @@ import numpy as np
 from geopy.geocoders import Nominatim
 
 geolocator = Nominatim()
+data = pd.DataFrame()
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -16,6 +17,17 @@ class InicioHandler(tornado.web.RequestHandler):
         df = self.df
         dic = datos_iniciales(df)
         self.write({"array" :dic})
+    
+    def initialize(self, df):
+        self.df = df
+
+class CuantosCiudadHandler(tornado.web.RequestHandler):
+    def get(self):
+        df = self.df
+        ciudad = self.get_argument("ciudad")
+        df_ciudad = cuantos_ciudad(ciudad, df)
+        dic = {'ciudad': ciudad, 'count': df_ciudad.shape[0]}
+        self.write({"array":dic})
     
     def initialize(self, df):
         self.df = df
@@ -46,16 +58,22 @@ def datos_iniciales(df):
     dic_locations['Longitud'] = lon
     return dic_locations
 
+def cuantos_ciudad(ciudad, df):
+    df_gets = df.loc[df['Location'].str.contains(ciudad)]
+    return df_gets
+
 if __name__ == "__main__":
     path = os.path.join(os.path.dirname(__file__), "../../Crashes.csv")
     print("Iniciando...")
     print("Cargando Datos...")
     df = pd.read_csv(path)
+    df['Location'].fillna('NaN', inplace=True)
     #df["time"] = pd.to_datetime(df.Timestamp, format="%Y-%m-%d %H:%M:%S")
     
     application = tornado.web.Application([
         (r"/", MainHandler),
         (r"/inicio", InicioHandler, {"df":df}),
+        (r"/ciudad", CuantosCiudadHandler, {"df":df}),
         (r"/static/(.*)", tornado.web.StaticFileHandler,
             {"path": settings["static_path"]})
 
--- va_air_crash/web_viewer/static/js/script_MapaInicial.js
+++ va_air_crash/web_viewer/static/js/script_MapaInicial.js
@@ -1,11 +1,20 @@
 /**
  * Script que se encarga de dibujar el mapa incial de la aplicación
  */
-mapaInicial()
+accidentes_por_ciudad("Paris")
 
 function mapaInicial() {        				
     console.log("Leyendo los datos...");        
     d3.json("inicio", function (e, d) {
+		console.log(d.array);
+        });	
+}
+
+function accidentes_por_ciudad(ciudad){
+	console.log("Buscando datos de " + ciudad + "...");
+	var url = "ciudad?"+
+				"ciudad="+ciudad;
+	d3.json(url, function (e, d) {
 		console.log(d.array);
         });	
 }
