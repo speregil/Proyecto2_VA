cd1de00d598fd6bc66181fdec1d30c48e7687db8
panico <panicroom91@gmail.com>
2015-06-21 17:33:50 -0500

    arreglo de tooltip muestra info de destino y accidente

--- va_air_crash/web_viewer/static/static/js/drawMap.js
+++ va_air_crash/web_viewer/static/static/js/drawMap.js
@@ -15,14 +15,14 @@ var path = d3.geo.path()
 
 var g = svg.append("g");
 var g2 = svg.append("g").attr("class","puntoDestino");
-						var g3 = svg.append("g").attr("class","puntoAccidente");
+var g3 = svg.append("g").attr("class","puntoAccidente");
 
 // load and display the World
-d3.json("topology", function(error, topology) {
+d3.json("world-110m2.json", function(error, topology) {
 
 // load and display the cities
-d3.json("inicio", function(error, data) {
-	g.selectAll("circle")
+d3.json("ciudades.json", function(error, data) {
+    g.selectAll("circle")
        .data(data)
        .enter()
        .append("circle")
@@ -34,57 +34,70 @@ d3.json("inicio", function(error, data) {
        })
        .attr("r", 5)
        .style("fill", "#F20057")
-	   .on("mouseover", function(d) {
+     .on("mouseover", function(d) {
 
-					//Get this bar's x/y values, then augment for the tooltip
-					var xPosition = parseFloat(d3.select(this).attr("x"));
-					var yPosition = parseFloat(d3.select(this).attr("y"));
+          //Get this bar's x/y values, then augment for the tooltip
+          var xPosition = parseFloat(d3.select(this).attr("cx"));
+          var yPosition = parseFloat(d3.select(this).attr("cy"));
 
-					//Update the tooltip position and value
-					d3.select("#tooltip")
-						.style("left", xPosition + "px")
-						.style("top", yPosition + "px")						
-						.select("#value")
-						.text(d.country);
-						
-												
-						g2.selectAll("circle")
-       						.data(data)
-       						.enter()
-       						.append("circle")
-							.attr("cx", function(d) {
-								return projection([d.lonDestino, d.latDestino])[0];
-							})
-							.attr("cy", function(d) {
-								   return projection([d.lonDestino, d.latDestino])[1];
-						   })
-						   .attr("r", 5);
-						g3.selectAll("circle")
-       						.data(data)
-       						.enter()
-       						.append("circle")
-							.attr("cx", function(d) {
-								return projection([d.lonAccidente, d.latAccidente])[0];
-							})
-							.attr("cy", function(d) {
-								   return projection([d.lonAccidente, d.latAccidente])[1];
-						   })
-						   .attr("r", 5);
-			   			
-					//Show the tooltip
-					d3.select("#tooltip").classed("hidden", false);
+          //Update the tooltip position and value
+          d3.select("#tooltip")
+            .style("left", xPosition + "px")
+            .style("top", yPosition + "px")           
+            .select("#value")
+            .text(d.city + ", " + d.country + "\n" + "destino: " + d.cityDestino + "\n" + "accidente: " + d.cityAccidente + "   "+ xPosition + " - " + yPosition);
+            
+                        
+            g2.selectAll("circle")
+                  .data(data)
+                  .enter()
+                  .append("circle")
+              .attr("cx", function(d) {
 
-			   })
-			   .on("mouseout", function() {
-				   
-				   g2.selectAll("circle").remove();
-			   g3.selectAll("circle").remove();
-					//Hide the tooltip
-					d3.select("#tooltip").classed("hidden", true);
-					
-			   });
-	   
-	   g.selectAll("text")
+                if (xPosition == projection([d.lonDestino, d.latDestino])[0]) {
+                  return projection([d.lonDestino, d.latDestino])[0];
+                };
+                
+              })
+              .attr("cy", function(d) {
+                if (yPosition == projection([d.lonDestino, d.latDestino])[1]) {
+                  return projection([d.lonDestino, d.latDestino])[1];
+                };
+                   
+               })
+               .attr("r", 5);
+            g3.selectAll("circle")
+                  .data(data)
+                  .enter()
+                  .append("circle")
+              .attr("cx", function(d) {
+                if (false) {
+                  return projection([d.lonAccidente, d.latAccidente])[0];
+                };
+                
+              })
+              .attr("cy", function(d) {
+                if (false) {
+                  return projection([d.lonAccidente, d.latAccidente])[1];
+                };
+                   
+               })
+               .attr("r", 5);
+              
+          //Show the tooltip
+          d3.select("#tooltip").classed("hidden", false);
+
+         })
+         .on("mouseout", function() {
+           
+           g2.selectAll("circle").remove();
+         g3.selectAll("circle").remove();
+          //Hide the tooltip
+          d3.select("#tooltip").classed("hidden", true);
+          
+         });
+     
+     g.selectAll("text")
        .data(data)
        .enter()
      .append("text") // append text
@@ -118,21 +131,21 @@ var zoom = d3.behavior.zoom()
             .attr("d", path.projection(projection));
         g.selectAll("path")  
             .attr("d", path.projection(projection)); 
-		
-		g2.attr("transform","translate("+ 
+    
+    g2.attr("transform","translate("+ 
             d3.event.translate.join(",")+")scale("+d3.event.scale+")");
         g2.selectAll("circle")
             .attr("d", path.projection(projection));
         g2.selectAll("path")  
             .attr("d", path.projection(projection)); 
-			
-		g3.attr("transform","translate("+ 
+      
+    g3.attr("transform","translate("+ 
             d3.event.translate.join(",")+")scale("+d3.event.scale+")");
         g3.selectAll("circle")
             .attr("d", path.projection(projection));
         g3.selectAll("path")  
             .attr("d", path.projection(projection)); 
-		
+    
 
   });
 
