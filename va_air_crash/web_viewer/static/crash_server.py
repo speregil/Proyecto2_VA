0ed3b7ad31d4a66d2eafb68307441c4e4612e43a
speregil <s.gilparga@gmail.com>
2015-06-21 16:14:18 -0500

    Conexion parcial con el servidor. Ya pinta la topologia

--- va_air_crash/web_viewer/static/crash_server.py
+++ va_air_crash/web_viewer/static/crash_server.py
@@ -2,11 +2,27 @@ import tornado.ioloop
 import tornado.web
 import os
 import pandas as pd
-import numpy as np
+import json
 from geopy.geocoders import Nominatim
 
 geolocator = Nominatim()
 data = pd.DataFrame()
+topology = None
+test = None
+template_topo = {
+    "city":"NaN",
+    "country":"NaN",
+    "lat":0,
+    "lon":0,
+	"cityDestino":"NaN",
+    "countryDestino":"NaN",
+    "latDestino":-15.67,
+    "lonDestino":-47.43,
+	"cityAccidente":"NaN",
+    "countryAccidente":"NaN",
+    "latAccidente":-4.09,
+    "lonAccidente":-70.00
+  }
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -15,8 +31,9 @@ class MainHandler(tornado.web.RequestHandler):
 class InicioHandler(tornado.web.RequestHandler):
     def get(self):
         df = self.df
-        dic = datos_iniciales(df)
-        self.write({"array" :dic})
+        #list_data = datos_iniciales(df)
+        #self.write({"array" :list_data})
+        self.write(test)
     
     def initialize(self, df):
         self.df = df
@@ -32,48 +49,74 @@ class CuantosCiudadHandler(tornado.web.RequestHandler):
     def initialize(self, df):
         self.df = df
         
+class TopologyHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write(topology)
+
 settings = {"template_path" : os.path.dirname(__file__),
             "static_path" : os.path.join(os.path.dirname(__file__),"static"),
             "debug" : True
             } 
 
 def datos_iniciales(df):
+    global template_topo
     locations = df['Location'].drop_duplicates()
-    df_small = locations.sample(100)
-    dic_locations = {}
-    lat = []
-    lon = []
-    nom = []
+    df_small = locations.sample(10)
+    list_data = []
     for index in df_small.iteritems():
         try:
             location = geolocator.geocode(index[1])
         except:
             pass
         if(not location is None):
-            nom.append(index[1])
-            lat.append(location.latitude)
-            lon.append(location.longitude)
-    dic_locations['Location'] = nom
-    dic_locations['Latitud'] = lat
-    dic_locations['Longitud'] = lon
-    return dic_locations
+            limpiar_template()
+            template_topo["city"] = index[1]
+            template_topo["lat"] = location.latitude
+            template_topo["lon"] = location.longitude
+            list_data.append(template_topo)
+    return list_data
 
 def cuantos_ciudad(ciudad, df):
     df_gets = df.loc[df['Location'].str.contains(ciudad)]
     return df_gets
 
+def limpiar_template():
+    global template_topo    
+    template_topo = {
+    "city":"NaN",
+    "country":"NaN",
+    "lat":0,
+    "lon":0,
+	"cityDestino":"NaN",
+    "countryDestino":"NaN",
+    "latDestino":-15.67,
+    "lonDestino":-47.43,
+	"cityAccidente":"NaN",
+    "countryAccidente":"NaN",
+    "latAccidente":-4.09,
+    "lonAccidente":-70.00
+    }
+    
 if __name__ == "__main__":
-    path = os.path.join(os.path.dirname(__file__), "../../Crashes.csv")
+    path = os.path.join(os.path.dirname(__file__), "../../../Crashes.csv")
     print("Iniciando...")
     print("Cargando Datos...")
     df = pd.read_csv(path)
     df['Location'].fillna('NaN', inplace=True)
+    with open("world-110m2.json") as json_file:
+        topology = json.load(json_file)
+        
+    with open("ciudades.json") as json_file:
+        test = json.load(json_file)
+    
+    
     #df["time"] = pd.to_datetime(df.Timestamp, format="%Y-%m-%d %H:%M:%S")
     
     application = tornado.web.Application([
         (r"/", MainHandler),
         (r"/inicio", InicioHandler, {"df":df}),
         (r"/ciudad", CuantosCiudadHandler, {"df":df}),
+        (r"/topology", TopologyHandler),
         (r"/static/(.*)", tornado.web.StaticFileHandler,
             {"path": settings["static_path"]})
 
---
+++ va_air_crash/web_viewer/static/static/js/drawMap.js
@@ -0,0 +1,139 @@
+var width = 960,
+    height = 500;
+
+var projection = d3.geo.mercator()
+    .center([0, 20 ])
+    .scale(150)
+    .rotate([0,0]);
+
+var svg = d3.select("body #mapa").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+var path = d3.geo.path()
+    .projection(projection);
+
+var g = svg.append("g");
+var g2 = svg.append("g").attr("class","puntoDestino");
+						var g3 = svg.append("g").attr("class","puntoAccidente");
+
+// load and display the World
+d3.json("topology", function(error, topology) {
+
+// load and display the cities
+d3.json("inicio", function(error, data) {
+	g.selectAll("circle")
+       .data(data)
+       .enter()
+       .append("circle")
+       .attr("cx", function(d) {
+               return projection([d.lon, d.lat])[0];
+       })
+       .attr("cy", function(d) {
+               return projection([d.lon, d.lat])[1];
+       })
+       .attr("r", 5)
+       .style("fill", "#F20057")
+	   .on("mouseover", function(d) {
+
+					//Get this bar's x/y values, then augment for the tooltip
+					var xPosition = parseFloat(d3.select(this).attr("x"));
+					var yPosition = parseFloat(d3.select(this).attr("y"));
+
+					//Update the tooltip position and value
+					d3.select("#tooltip")
+						.style("left", xPosition + "px")
+						.style("top", yPosition + "px")						
+						.select("#value")
+						.text(d.country);
+						
+												
+						g2.selectAll("circle")
+       						.data(data)
+       						.enter()
+       						.append("circle")
+							.attr("cx", function(d) {
+								return projection([d.lonDestino, d.latDestino])[0];
+							})
+							.attr("cy", function(d) {
+								   return projection([d.lonDestino, d.latDestino])[1];
+						   })
+						   .attr("r", 5);
+						g3.selectAll("circle")
+       						.data(data)
+       						.enter()
+       						.append("circle")
+							.attr("cx", function(d) {
+								return projection([d.lonAccidente, d.latAccidente])[0];
+							})
+							.attr("cy", function(d) {
+								   return projection([d.lonAccidente, d.latAccidente])[1];
+						   })
+						   .attr("r", 5);
+			   			
+					//Show the tooltip
+					d3.select("#tooltip").classed("hidden", false);
+
+			   })
+			   .on("mouseout", function() {
+				   
+				   g2.selectAll("circle").remove();
+			   g3.selectAll("circle").remove();
+					//Hide the tooltip
+					d3.select("#tooltip").classed("hidden", true);
+					
+			   });
+	   
+	   g.selectAll("text")
+       .data(data)
+       .enter()
+     .append("text") // append text
+       .attr("x", function(d) {
+               return projection([d.lon, d.lat])[0];
+       })
+       .attr("y", function(d) {
+               return projection([d.lon, d.lat])[1];
+       })
+       .attr("dy", -7) // set y position of bottom of text
+      .style("fill", "black") // fill the text with the colour black
+      .attr("text-anchor", "middle") // set anchor y justification
+      .text(function(d) {return d.city;}); // define the text to display
+});
+
+
+g.selectAll("path")
+      .data(topojson.feature(topology, topology.objects.countries)
+.features)
+    .enter()
+      .append("path")
+      .attr("d", path)
+});
+
+// zoom and pan
+var zoom = d3.behavior.zoom()
+    .on("zoom",function() {
+        g.attr("transform","translate("+ 
+            d3.event.translate.join(",")+")scale("+d3.event.scale+")");
+        g.selectAll("circle")
+            .attr("d", path.projection(projection));
+        g.selectAll("path")  
+            .attr("d", path.projection(projection)); 
+		
+		g2.attr("transform","translate("+ 
+            d3.event.translate.join(",")+")scale("+d3.event.scale+")");
+        g2.selectAll("circle")
+            .attr("d", path.projection(projection));
+        g2.selectAll("path")  
+            .attr("d", path.projection(projection)); 
+			
+		g3.attr("transform","translate("+ 
+            d3.event.translate.join(",")+")scale("+d3.event.scale+")");
+        g3.selectAll("circle")
+            .attr("d", path.projection(projection));
+        g3.selectAll("path")  
+            .attr("d", path.projection(projection)); 
+		
+
+  });
+
+svg.call(zoom)
